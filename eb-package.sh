8035d99abfe8be0a3e0fc7fb4d0fbc06fefc4da6
John Price <joprice@ea.com>
2015-12-04 11:18:35 -0500

    Fix packaging script

--- eb-package.sh
+++ eb-package.sh
@@ -1,7 +1,12 @@
 rm -rf package
+rm package.zip
 mkdir package
 mvn package
 cp target/status.jar package
 cp etc/Procfile package
 cp etc/Buildfile package
-zip package
+cp *.yml package
+cd package
+zip package *
+mv package.zip ..
+cd ..
